bfda900cb031e6dc516c9cbad8fe189c461ec180
Unknown <crisgonmon@hotmail.com>
2018-05-30 20:56:07 +0200

    debugging on modals and scrolling added

--- logic.js
+++ logic.js
@@ -1,115 +1,139 @@
-
+var noticiacount = 0;
 $(document).ready(function () {
     $.getJSON("https://raw.githubusercontent.com/Aliena28898/Infinite-scrolling-news-webpage/8c584b7/newsdefault.json", function (data) {
         //en aquest punt l'objecte jsonObject correspon al fitxer
         pintar(data);
     });
+    $.getJSON("https://raw.githubusercontent.com/Aliena28898/Infinite-scrolling-news-webpage/8c584b7/newsdefault.json", function (data2) {
+        //en aquest punt l'objecte jsonObject correspon al fitxer
+
+        $(window).scroll(function () {
+            addNews(data2);
+        });
+    });
 });
 
 
+function addNews(data) {
+    var arr = [];
+    for (var i = noticiacount; i < noticiacount + 3; i++) {
+        if (i < data.length) {
+            arr.push(data[i]);
+        }
+    }
+    noticiacount += 3;
+    pintar(arr);
+}
 
-
-function pintar(json){
+function pintar(json) {
     var arr = json;
 
     for (var i = 0; i < arr.length; i++) {
 
-    var noticias = $('#newsContainer');
-    var div = document.createElement('div');
-    div.setAttribute('class','noticia');
-    div.setAttribute('data-toggle','modal');
-    div.setAttribute('data-target','#'+arr[i].id);
-    noticias.append(div);
+        var noticias = $('#newsContainer');
+        var div = document.createElement('div');
+        div.setAttribute('class', 'noticia');
+        div.setAttribute('data-toggle', 'modal');
+        div.setAttribute('data-target', '#' + arr[i].id);
+        div.setAttribute('onclick', 'openModal('+ arr[i].id+')');
+        noticias.append(div);
 
-    var img = document.createElement('img');
-    img.setAttribute("src",arr[i].imgbig);
-    img.setAttribute("class","imgTitle");
-    div.appendChild(img);
+        var img = document.createElement('img');
+        img.setAttribute("src", arr[i].imgbig);
+        img.setAttribute("class", "imgTitle");
+        div.appendChild(img);
 
-    var title = document.createElement('h2');
-    title.innerHTML=arr[i].title;
-    title.setAttribute("class","noticiaTitle");
-    div.appendChild(title);
+        var title = document.createElement('h2');
+        title.innerHTML = arr[i].title;
+        title.setAttribute("class", "noticiaTitle");
+        div.appendChild(title);
 
-    var subtitle = document.createElement('p');
-    subtitle.innerHTML=arr[i].subtitle;
-    subtitle.setAttribute("class","noticiaSubTitle");
-    div.appendChild(subtitle);
+        var subtitle = document.createElement('p');
+        subtitle.innerHTML = arr[i].subtitle;
+        subtitle.setAttribute("class", "noticiaSubTitle");
+        div.appendChild(subtitle);
 
-    var modal= document.createElement('div');
-    modal.setAttribute("id", arr[i].id);
-    modal.setAttribute("class", "modal fade");
-    modal.setAttribute("role","dialog");
-    noticias.append(modal);
-    
-    var modaldia = document.createElement('div');
-    modaldia.setAttribute("class","modal-content");
-    modal.appendChild(modaldia);
+        var modal = document.createElement('div');
+        modal.setAttribute("id", arr[i].id);
+        modal.setAttribute("class", "modal fade");
+        modal.setAttribute("role", "dialog");
+        noticias.append(modal);
 
-    var modalhead = document.createElement('div');
-    modalhead.setAttribute("class","modal-header");
-    modaldia.appendChild(modalhead);
+        var modaldia = document.createElement('div');
+        modaldia.setAttribute("class", "modal-content");
+        modal.appendChild(modaldia);
 
-    var modtitle = document.createElement('h4');
-    modtitle.setAttribute("class","modal-title");
-    modtitle.innerHTML = arr[i].title;
-    modalhead.appendChild(modtitle);
+        var modalhead = document.createElement('div');
+        modalhead.setAttribute("class", "modal-header");
+        modaldia.appendChild(modalhead);
 
-    var button = document.createElement('button');
-    button.setAttribute("class","close");
-    button.setAttribute("data-dismiss", "modal");
-    button.innerHTML = "&times;";
-    modalhead.appendChild(button);
+        var modtitle = document.createElement('h4');
+        modtitle.setAttribute("class", "modal-title");
+        modtitle.innerHTML = arr[i].title;
+        modalhead.appendChild(modtitle);
 
-    var modalbody = document.createElement('div');
-    modalbody.setAttribute("class","modal-body");
-    modaldia.appendChild(modalbody);
+        var button = document.createElement('button');
+        button.setAttribute("class", "close");
+        button.setAttribute("onclick", "closeModal(" + arr[i].id + ")");
+        button.innerHTML = "&times;";
+        modalhead.appendChild(button);
 
-    var modalimgbig = document.createElement('img');
-    modalimgbig.setAttribute("src",arr[i].imgbig);
-    modalimgbig.setAttribute("class","imgTitle");
-    modalbody.appendChild(modalimgbig);
+        var modalbody = document.createElement('div');
+        modalbody.setAttribute("class", "modal-body");
+        modaldia.appendChild(modalbody);
 
-    modalsubtitle = document.createElement('p');
-    modalsubtitle.innerHTML=arr[i].subtitle;
-    modalsubtitle.setAttribute("class","noticiaSubTitle");
-    modalbody.appendChild(modalsubtitle);
+        var modalimgbig = document.createElement('img');
+        modalimgbig.setAttribute("src", arr[i].imgbig);
+        modalimgbig.setAttribute("class", "imgTitle");
+        modalbody.appendChild(modalimgbig);
 
-    var modalimgmid = document.createElement('img');
-    modalimgmid.setAttribute("src",arr[i].imgmid);
-    modalimgmid.setAttribute("class","imgmid");
-    modalbody.appendChild(modalimgmid);
+        modalsubtitle = document.createElement('p');
+        modalsubtitle.innerHTML = arr[i].subtitle;
+        modalsubtitle.setAttribute("class", "noticiaSubTitle");
+        modalbody.appendChild(modalsubtitle);
 
-    var modaltext = document.createElement('p');
-    modaltext.innerHTML=arr[i].text;
-    modaltext.setAttribute("class","noticiatext");
-    modalbody.appendChild(modaltext);
+        var modalimgmid = document.createElement('img');
+        modalimgmid.setAttribute("src", arr[i].imgmid);
+        modalimgmid.setAttribute("class", "imgmid");
+        modalbody.appendChild(modalimgmid);
 
-    var video = document.createElement('iframe');
-    var url = arr[i].video;
-    url = url.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/");
-    video.setAttribute("src",url);
-    video.setAttribute('class','video');
-    modalbody.appendChild(video);
+        var modaltext = document.createElement('p');
+        modaltext.innerHTML = arr[i].text;
+        modaltext.setAttribute("class", "noticiatext");
+        modalbody.appendChild(modaltext);
 
-    var modalfooter = document.createElement('div');
-    modalfooter.setAttribute("class","modal-footer");
-    modaldia.appendChild(modalfooter);
+        var video = document.createElement('iframe');
+        var url = arr[i].video;
+        url = url.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/");
+        video.setAttribute("src", url);
+        video.setAttribute('class', 'video');
+        modalbody.appendChild(video);
 
-    var buttonfoot = document.createElement('a');
-    buttonfoot.setAttribute("href","#");
-    buttonfoot.setAttribute("class","close");
-    buttonfoot.setAttribute("data-dismiss","modal");
-    buttonfoot.setAttribute("aria-label","close");
-    buttonfoot.innerHTML = "Cerrar";
-    modalfooter.appendChild(buttonfoot);
+        var modalfooter = document.createElement('div');
+        modalfooter.setAttribute("class", "modal-footer");
+        modaldia.appendChild(modalfooter);
+
+        var buttonfoot = document.createElement('button');
+        buttonfoot.setAttribute("class", "close");
+        buttonfoot.setAttribute("onclick", "closeModal(" + arr[i].id + ")");
+        buttonfoot.innerHTML = "Cerrar";
+        modalfooter.appendChild(buttonfoot);
     }
 }
 
-function closeModal(id){
-    $('#'+id).style="display: none;";
+function openModal(id){
+    document.getElementById(id).style.display="initial";
+}
+function closeModal(id) {
+    //document.getElementById(id).removeClass("in");
+    $(".modal-backdrop").remove();
+    $('body').removeClass('modal-open');
+    $('body').css('padding-right', '');
+    document.getElementById(id).style.display = "none";
 }
 
-function abrirNoticia(id){
+function abrirNoticia(id) {
     window.open('noticia.html', '_blank');
 }
+
+
